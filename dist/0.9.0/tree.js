39849cd7609c22a4e2b32f51f41e8c00c9ce4688
Marco Schwarz <schwarz.marco@voestalpine.com>
2025-03-20 10:56:07 +0000

    Fix rendering

--- dist/0.9.0/tree.js
+++ dist/0.9.0/tree.js
@@ -1,14 +1,17 @@
-const sheet = new CSSStyleSheet();sheet.replaceSync("\n");
+const sheet = new CSSStyleSheet();sheet.replaceSync("\n.cont-children {\n    padding-left: 20px;\n    position: relative;\n    display: block;\n\n    &::before {\n        content: '';\n        width: 0;\n        height: 100%;\n        position: absolute;\n        border: 1px solid lightgrey;\n        top: 0;\n        left: 11px;\n    }\n\n    /* & .list-elem::before { */\n        /* content: \"|\"; */\n        /* padding-right: 5px; */\n    /* } */\n}\n\n.list-elem {\n    padding: 8px 10px;\n    cursor: pointer;\n\n    &:hover {\n        background-color: whitesmoke;\n    }\n}\n\ndiv[type=\"root\"] {\n    & > .list-elem::before {\n        transition: transform 0.1s;\n        font-family: \"Font Awesome 5 Pro\";\n        content: \"\\f105\";\n        color: grey;\n        padding-right: 5px;\n        display: inline-block;\n        transform: rotate(90deg) translate(3px, 3px);\n    }\n}\n\ndiv[closed] {\n    & > .cont-children {\n        display: none;\n    }\n\n    & > .list-elem::before {\n        transition: transform 0.1s;\n        transform: rotate(0deg) translate(0, 0);\n    }\n}\n");
 
 class HeliumTree extends HTMLElement {
     static observedAttributes = [
     ];
+    /** @type {HTMLDivElement} */
+    $contItems;
 
     constructor() {
         super();
         let shadow = this.attachShadow({ mode: "open" });
-
-        shadow.append(this.$slot);
+        this.$contItems = document.createElement('div');
+        this.$contItems.id = 'cont-items';
+        shadow.append(this.$contItems);
         shadow.adoptedStyleSheets = [sheet];
     }
 
@@ -23,45 +26,100 @@ class HeliumTree extends HTMLElement {
 
     connectedCallback() {
         let items = [];
-        for (let $elem of this.children) {
-            $e = this._renderLeaf($elem);
-            items.push($e);
+        for (let $elem of Array.from(this.children)) {
+            let $item = this._renderLeaf($elem);
+            this.$contItems.append($item);
+            items.push($item);
         }
 
-        for (let $item of this.children) {
+        for (let $elem of items) {
             const selParent = $elem.getAttribute('parent');
             if (selParent == null) {
-                this._renderLeaf($elem);
+                this.$contItems.append($elem);
                 continue;
             }
 
-            const $parent = document.querySelector(selParent);
+            const $parent = this.$contItems.querySelector('#' + selParent);
             if ($parent == null) {
-                throw new Error('No parent found with selector ' + selParent);
+                throw new Error('No parent found with selector #' + selParent);
             }
 
-            if (!$parent.isRoot) {
-                $parent.append($elem);
+            if ($parent.getAttribute('type') !== 'root') {
+                $parent.setAttribute('type', 'root');
+                let $contChildren = document.createElement('div');
+                $contChildren.classList.add('cont-children');
+                $contChildren.append($elem);
+                $parent.append($contChildren);
+                $parent.children[0].addEventListener('click', () => this._clickRootCallback.bind(this)($parent));
+            } else {
+                $parent.children[1].append($elem);
             }
         }
+    }
+
+    filter(filterText) {
+        for(const $elem of this.$contItems.children) {
+            this._filterRecursive($elem, filterText);
+        }
+    }
+
+    // TODO(marco): Filter based on node type
+    // TODO(marco): Show decendants when root is shown
+    _filterRecursive($item, filterText) {
+        let show = filterText == null;
+        if ($item.getAttribute('type') === 'root') {
+            for(const $elem of $item.children[1].children) {
+                show ||= this._filterRecursive($elem, filterText);
+            }
+        }
+
+        show ||= $item.children[0].innerHTML.includes(filterText);
+        if (show) {
+            $item.style.display = '';
+        } else {
+            $item.style.display = 'none';
+            $item.setAttribute('closed', 'true');
+        }
+
+        return show;
+    }
+
+    _clickRootCallback($elem) {
+        this._toggleChildren($elem);
+    }
+
+    _toggleChildren($elem) {
+        if ($elem.getAttribute('closed')) {
+            this._showChildren($elem);
+        } else {
+            this._hideChildren($elem);
+        }
+    }
+
+    _hideChildren($elem) {
+        $elem.setAttribute('closed', 'true');
+    }
+
+    _showChildren($elem) {
+        $elem.removeAttribute('closed');
     }
 
     /**
      * @param {HTMLElement} $elem 
      */
-    _renderRoot($elem) {
+    _renderLeaf($elem) {
         let $cont = document.createElement('div');
-        $cont.class = 'cont-root';
+        const parent = $elem.getAttribute('parent');
+        if (parent != null) {
+            $cont.setAttribute('parent', parent);
+        }
+        $cont.id = $elem.id;
+        $cont.classList.add('cont-elem');
+        $elem.id = '';
+        $elem.classList.add('list-elem');
+        $cont.setAttribute('type', 'leaf');
         $cont.append($elem);
-        let $contChildren = document.createElement('div');
-        $cont.class = 'cont-children';
-        $cont.append($contChildren);
-    }
-
-    _renderLeaf() {
-        let $cont = document.createElement('div');
-        $cont.class = 'cont-leaf';
-        $cont.append($elem);
+        return $cont;
     }
 
 }
--- src/tree.js
+++ src/tree.js
@@ -3,12 +3,15 @@ import sheet from './tree.css';
 export class HeliumTree extends HTMLElement {
     static observedAttributes = [
     ];
+    /** @type {HTMLDivElement} */
+    $contItems;
 
     constructor() {
         super();
         let shadow = this.attachShadow({ mode: "open" });
-
-        shadow.append(this.$slot);
+        this.$contItems = document.createElement('div');
+        this.$contItems.id = 'cont-items';
+        shadow.append(this.$contItems);
         shadow.adoptedStyleSheets = [sheet];
     }
 
@@ -27,45 +30,100 @@ export class HeliumTree extends HTMLElement {
 
     connectedCallback() {
         let items = [];
-        for (let $elem of this.children) {
-            $e = this._renderLeaf($elem);
-            items.push($e);
+        for (let $elem of Array.from(this.children)) {
+            let $item = this._renderLeaf($elem);
+            this.$contItems.append($item);
+            items.push($item)
         }
 
-        for (let $item of this.children) {
+        for (let $elem of items) {
             const selParent = $elem.getAttribute('parent');
             if (selParent == null) {
-                this._renderLeaf($elem);
+                this.$contItems.append($elem);
                 continue;
             }
 
-            const $parent = document.querySelector(selParent);
+            const $parent = this.$contItems.querySelector('#' + selParent);
             if ($parent == null) {
-                throw new Error('No parent found with selector ' + selParent);
+                throw new Error('No parent found with selector #' + selParent);
             }
 
-            if (!$parent.isRoot) {
-                $parent.append($elem);
+            if ($parent.getAttribute('type') !== 'root') {
+                $parent.setAttribute('type', 'root');
+                let $contChildren = document.createElement('div');
+                $contChildren.classList.add('cont-children');
+                $contChildren.append($elem);
+                $parent.append($contChildren);
+                $parent.children[0].addEventListener('click', () => this._clickRootCallback.bind(this)($parent));
+            } else {
+                $parent.children[1].append($elem);
             }
         }
+    }
+
+    filter(filterText) {
+        for(const $elem of this.$contItems.children) {
+            this._filterRecursive($elem, filterText);
+        }
+    }
+
+    // TODO(marco): Filter based on node type
+    // TODO(marco): Show decendants when root is shown
+    _filterRecursive($item, filterText) {
+        let show = filterText == null;
+        if ($item.getAttribute('type') === 'root') {
+            for(const $elem of $item.children[1].children) {
+                show ||= this._filterRecursive($elem, filterText);
+            }
+        }
+
+        show ||= $item.children[0].innerHTML.includes(filterText);
+        if (show) {
+            $item.style.display = '';
+        } else {
+            $item.style.display = 'none';
+            $item.setAttribute('closed', 'true');
+        }
+
+        return show;
+    }
+
+    _clickRootCallback($elem) {
+        this._toggleChildren($elem);
+    }
+
+    _toggleChildren($elem) {
+        if ($elem.getAttribute('closed')) {
+            this._showChildren($elem);
+        } else {
+            this._hideChildren($elem);
+        }
+    }
+
+    _hideChildren($elem) {
+        $elem.setAttribute('closed', 'true');
+    }
+
+    _showChildren($elem) {
+        $elem.removeAttribute('closed');
     }
 
     /**
      * @param {HTMLElement} $elem 
      */
-    _renderRoot($elem) {
+    _renderLeaf($elem) {
         let $cont = document.createElement('div');
-        $cont.class = 'cont-root';
+        const parent = $elem.getAttribute('parent');
+        if (parent != null) {
+            $cont.setAttribute('parent', parent);
+        }
+        $cont.id = $elem.id;
+        $cont.classList.add('cont-elem');
+        $elem.id = '';
+        $elem.classList.add('list-elem');
+        $cont.setAttribute('type', 'leaf');
         $cont.append($elem);
-        let $contChildren = document.createElement('div');
-        $cont.class = 'cont-children';
-        $cont.append($contChildren);
-    }
-
-    _renderLeaf() {
-        let $cont = document.createElement('div');
-        $cont.class = 'cont-leaf';
-        $cont.append($elem);
+        return $cont;
     }
 
 }
