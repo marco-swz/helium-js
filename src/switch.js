80b01b6dd4fb76bfe4d6f1f415f665868238810d
Gasparin David <D.Gasparin@voestalpine.com>
2025-07-03 15:25:19 +0200

    not yet finished implementation of JS file

---
+++ src/switch.js
@@ -0,0 +1,35 @@
+export class HeliumSwitch extends HTMLElement {
+
+    static observedAttributes = ["checked"];
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+    }
+
+    connectedCallback() {
+        // Hier muss ich noch den Stylesheet aufrufen
+
+
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        // if an observerd attribute is to be added
+        if (name == "checked") {
+
+            const toggle = this.shadowRoot.querySelector(".switch");
+
+            if (this.hasAttribute("checked")) {
+                toggle.classList.add("checked");
+            } else {
+                toggle.classList.remove("checked")
+            }
+        }
+    }
+
+
+}
+
+if (!customElements.get('he-switch')) {
+    customElements.define("he-switch", HeliumSwitch);
+}
