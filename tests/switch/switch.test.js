aafcee1848009333c89e5f4f77a31f65a69d2801
Gasparin David <D.Gasparin@voestalpine.com>
2025-07-10 10:08:52 +0200

    added a few tests testing the apperance of the switch

--- tests/switch/switch.test.js
+++ tests/switch/switch.test.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { TIMEOUT } from 'dns';
 
 test.beforeEach(async({ page }) => {
     await page.goto('http://localhost:8080/tests/switch');
@@ -27,13 +28,51 @@ test("Should toggle the switch on click", async({ page }) => {
 });
 
 test("The internals should have checked when the switch is activated", async({ page }) => {
-
     const element = page.locator("#switch-test-click");
     await element.click();
     await expect(element).toHaveAttribute("checked");
-
     const hasChecked = await page.$eval("#switch-test-click", (el) => {
         return el.$internals.states.has("checked");
     });
     expect(hasChecked).toBe(true);
+});
+
+
+test("The switch should have the right color when it's on", async({ page }) => {
+    const component = await page.locator("#switch-test-click");
+    await component.click();
+    await expect(component).toHaveAttribute("checked");
+    await page.waitForTimeout(1000);
+    const color = await component.evaluate((el) => {
+        const innerElement = el.shadowRoot.querySelector(".slider");
+        return window.getComputedStyle(innerElement).backgroundColor;
+    });
+    expect(color).toBe("rgb(243, 131, 33)");
+});
+
+test("The switch should have the right color when it's off", async({ page }) => {
+    const component = await page.locator("#switch-test-click");
+    const color = await component.evaluate((el) => {
+        const innerElement = el.shadowRoot.querySelector(".slider");
+        return window.getComputedStyle(innerElement).backgroundColor;
+    });
+    expect(color).toBe("rgb(204, 204, 204)");
+});
+
+test("The switch should have the right width", async({ page }) => {
+    const component = await page.locator("#switch-test-click");
+    const width = await component.evaluate((el) => {
+        const innerElement = el.shadowRoot.querySelector(".switch");
+        return window.getComputedStyle(innerElement).width;
+    });
+    expect(width).toBe("60px");
+});
+
+test("The switch should the right height", async({ page }) => {
+    const component = await page.locator("#switch-test-click");
+    const height = await component.evaluate((el) => {
+        const innerElement = el.shadowRoot.querySelector(".switch");
+        return window.getComputedStyle(innerElement).height;
+    });
+    expect(height).toBe("34px");
 });
